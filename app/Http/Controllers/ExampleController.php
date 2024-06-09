08edc0e890b52026da5beeae232562d465838b71
conn-do <kondo@japaan.jp>
2024-06-09 18:42:57 +0900

    /example ページを作成

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Post;
+
+class ExampleController extends Controller
+{
+    public function index()
+    {
+        $post = Post::first();
+        return  view('example', [
+            'post' => $post
+        ]);
+    }
+}
