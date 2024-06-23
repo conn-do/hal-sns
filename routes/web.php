817f8d5317166e633d083ca9ff54bef56641f3a5
conn-do <kondo@japaan.jp>
2024-06-23 20:02:30 +0900

    Install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/example', [ExampleController::class, 'index']);
-Route::get('/', [PostController::class, 'index']);
-Route::get('/post/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/post', [PostController::class, 'post']);
-Route::post('/post', [PostController::class, 'save'])->name('posts.save');
-Route::get('/post/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('/post/{id}/edit', [PostController::class, 'update'])->name('posts.update');
-Route::get('/post/{postId}/comment', [CommentController::class, 'post'])->name('comments.post');
-Route::post('/post/{postId}/comment', [CommentController::class, 'save'])->name('comments.save');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
