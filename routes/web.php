eece62c93bf0022c5533e8dd86fc136c7cedf6bf
conn-do <kondo@japaan.jp>
2024-06-23 22:20:04 +0900

    Install breeze with react

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-use App\Http\Controllers\ExampleController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\CommentController;
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/example', [ExampleController::class, 'index']);
-Route::get('/', [PostController::class, 'index']);
-Route::get('/post/{id}', [PostController::class, 'show'])->name('posts.show');
-Route::get('/post', [PostController::class, 'post']);
-Route::post('/post', [PostController::class, 'save'])->name('posts.save');
-Route::get('/post/{id}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::post('/post/{id}/edit', [PostController::class, 'update'])->name('posts.update');
-Route::get('/post/{postId}/comment', [CommentController::class, 'post'])->name('comments.post');
-Route::post('/post/{postId}/comment', [CommentController::class, 'save'])->name('comments.save');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
