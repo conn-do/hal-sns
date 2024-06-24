d55d5c93d1b450345dda58b082bd725640596a67
conn-do <kondo@japaan.jp>
2024-06-24 11:01:33 +0900

    1限目の内容

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,23 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\CommentController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/example', [ExampleController::class, 'index']);
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
 Route::get('/', [PostController::class, 'index']);
 Route::get('/post/{id}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/post', [PostController::class, 'post']);
